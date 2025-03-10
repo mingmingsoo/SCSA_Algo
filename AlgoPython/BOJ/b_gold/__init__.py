03112286569a80c7e7eb9d195ca2623e7aa45e32
hyejoon <hjoon789@gmail.com>
2025-03-11 07:23:33 +0900

    1

--- AlgoPython/BOJ/b_gold/__init__.py
+++ AlgoPython/BOJ/b_gold/__init__.py
@@ -1,117 +1,115 @@
+'''
+문제설명
+    C->C
+    방향전환을 최소로
+구상
+    bfs같당 원래의 d를 데리고다녀야함!
+'''
 import heapq
-from collections import deque
 
-# print(4 * 4 * 4 * 4 * 4 * 5 * 4 * 3 * 2 * 1 * 4) # 491520
-# 회전시킨다.
+m, n = map(int, input().split())
+grid = [list(input()) for i in range(n)]
 
-hyper = []
+sr, sc, er, ec = -1, -1, -1, -1
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == "C":
+            grid[i][j] = "."
+            if sr == sc == -1:
+                sr, sc = i, j
+            else:
+                er, ec = i, j
 
-for w in range(5):
-    grid = [list(map(int, input().split())) for i in range(5)]
-    hyper.append(grid)
-
-h_list = []
-visited = [False] * 5
-sel = [0] * 5
-ans = 5 * 5 * 5 + 1
-
-
-def perm(idx):
-    if idx == 5:
-        h_list.append(sel[:])
-        return
-    for i in range(5):
-        if not visited[i]:
-            visited[i] = True
-            sel[idx] = i
-            perm(idx + 1)
-            visited[i] = False
+row = [-1, 0, 1, 0]
+col = [0, 1, 0, -1]
+ans = int(1e9)
 
 
-perm(0)
-# print(h_list)
-hei = [1, -1, 0, 0, 0, 0]
-row = [0, 0, 1, -1, 0, 0]
-col = [0, 0, 0, 0, 1, -1]
-
-
-def bfs(h, r, c, eh, er, ec, hyper):
+def bfs():
     global ans
-    visited = [[[False] * 5 for i in range(5)] for i in range(5)]
-    visited[h][r][c] = True
-
-    q = deque([(0, h, r, c)])
+    di = [[[int(1e9)] * 4 for i in range(m)] for i in range(n)]
+    di[sr][sc][0] = 0
+    di[sr][sc][1] = 0
+    di[sr][sc][2] = 0
+    di[sr][sc][3] = 0
+    q = []
+    heapq.heappush(q, (0, sr, sc, 0))
+    heapq.heappush(q, (0, sr, sc, 1))
+    heapq.heappush(q, (0, sr, sc, 2))
+    heapq.heappush(q, (0, sr, sc, 3))
 
     while q:
-        dist, h, r, c = q.popleft()
-        if dist >= ans:
-            return
-        if h == eh and c == ec and r == er:
-            ans = min(ans, dist)
-            return
-        for k in range(6):
-            nh = h + hei[k]
-            nr = r + row[k]
-            nc = c + col[k]
-            if not (0 <= nh < 5 and 0 <= nr < 5 and 0 <= nc < 5) or visited[nh][nr][nc]:
-                continue
-            if hyper[nh][nr][nc] == 1:
-                visited[nh][nr][nc] = True
-                q.append((dist + 1, nh, nr, nc))
+        rotation, r, c, d = heapq.heappop(q)
+        # print(r, c, d, rotation)
+        # print("--------------------")
+        # for _ in di:
+        #     print("".join(map(str, _)))
+        if rotation > di[r][c][d]:
+            continue
+        if r == er and c == ec:
+            ans = min(ans, rotation)
+            break
+
+        nr = r + row[d]
+        nc = c + col[d]
+        # 그냥 갈 수 있으면 그냥 퍼져가~~
+        if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == ".":
+            if di[nr][nc][d] > rotation:
+                di[nr][nc][d] = rotation
+                heapq.heappush(q, (rotation, nr, nc, d))
+
+            right_nr = nr + row[(d + 1) % 4]
+            right_nc = nc + col[(d + 1) % 4]
+            left_nr = nr + row[(d - 1) % 4]
+            left_nc = nc + col[(d - 1) % 4]
+
+            if 0 <= right_nr < n and 0 <= right_nc < m and grid[right_nr][right_nc] == ".":
+                if di[right_nr][right_nc][(d + 1) % 4] > rotation + 1:
+                    heapq.heappush(q, (rotation + 1, nr, nc, (d + 1) % 4))
+                    di[nr][nc][(d + 1) % 4] = rotation + 1
+
+            if 0 <= left_nr < n and 0 <= left_nc < m and grid[left_nr][left_nc] == ".":
+                if di[left_nr][left_nc][(d - 1) % 4] > rotation + 1:
+                    heapq.heappush(q, (rotation + 1, nr, nc, (d - 1) % 4))
+                    di[nr][nc][(d - 1) % 4] = rotation + 1
 
 
-def rotate(hyper, idx, origin_hyper):
-    for i in range(5):
-        for j in range(5):
-            hyper[idx][i][j] = origin_hyper[idx][j][5 - i - 1]
+bfs()
+print(ans)
+'''
+5 5
+C....
+####.
+..C#.
+.###.
+.....
+
+4 1
+C..C
+
+2 1
+CC
+
+6 5
+C.....
+#####.
+...C#.
+.####.
+......
+
+5 6
+C....
+####.
+..C#.
+.###.
+.###.
+.....
 
 
-def btk(idx):
-    if idx == 5:
-        for h_arr in h_list:
-            new_hyper = []
-            for h in h_arr:
-                new_hyper.append(hyper[h])
-            # print("---------------------")
-            # for _ in new_hyper:
-            #     print(_)
-            if new_hyper[0][0][0] == 1:
-                bfs(0, 0, 0, 4, 4, 4, new_hyper)
-            if new_hyper[0][4][0] == 1:
-                bfs(0, 4, 0, 4, 0, 4, new_hyper)
-            if new_hyper[0][4][4] == 1:
-                bfs(0, 4, 4, 4, 0, 0, new_hyper)
-            if new_hyper[0][0][4] == 1:
-                bfs(0, 0, 4, 4, 4, 0, new_hyper)
-        return
 
-    # 회전 없이 간다.
-    btk(idx + 1)
+4 3
+..*C
+C.*.
+*...
 
-    origin_hyper = [[ii[:] for ii in jj] for jj in hyper]
-    # for h in range(5):
-    #     for i in range(5):
-    #         for j in range(5):
-    #             origin_hyper[h][i][j] = hyper[h][i][j]
-
-    # 1번 회전
-    rotate(hyper, idx, origin_hyper)
-    btk(idx + 1)
-
-    # 2번 회전
-    origin_hyper = [[ii[:] for ii in jj] for jj in hyper]
-    rotate(hyper, idx, origin_hyper)
-
-    btk(idx + 1)
-
-    # 3번 회전
-    origin_hyper = [[ii[:] for ii in jj] for jj in hyper]
-    rotate(hyper, idx, origin_hyper)
-    btk(idx + 1)
-
-
-btk(0)
-if ans == 126:
-    print(-1)
-else:
-    print(ans)
+'''
